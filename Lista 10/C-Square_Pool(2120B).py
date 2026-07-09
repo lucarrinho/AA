976e341e8a8fbfd8292d89081ec0ee523f8535d0
kinhas <lucassmsilva878@gmail.com>
2026-07-09 13:54:59 -0300

    mexi

--- Lista 10/C-Square_Pool(2120B).py
+++ Lista 10/C-Square_Pool(2120B).py
@@ -3,8 +3,12 @@ for _ in range(int(input())):
     acc = 0
     for _ in range(n):
         dx, dy, x, y = map(int, input().split())
-        if (0-s)*(y-0)==(x-0)*(0-s):
+        if (0-x)*(y+dy-0)==(x+dx-0)*(0-y):
             acc += 1
-        elif (0-s)*(y-s)==(x-0)*(s-0):
+        elif (0-x)*(y+dy-s)==(x+dx-0)*(s-y):
+            acc += 1
+        elif (s-x)*(y+dy-0)==(x+dx-s)*(0-y):
+            acc += 1
+        elif (s-x)*(y+dy-s)==(x+dx-s)*(s-y):
             acc += 1
     print(acc)
